69c44d548aa093bae7120e69552826f5e4bf5a63
Enderson Marín <marinenderson1@gmail.com>
2022-09-27 14:02:14 -0500

    switchMap RXJS

--- src/app/paises/pages/ver-pais/ver-pais.component.ts
+++ src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,25 +9,27 @@ import { PaisService } from '../../services/pais.service';
   styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
   constructor(
     private activatedRoute: ActivatedRoute,
-    private paisService:PaisService
-    ) {
-
-    }
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    // this.activatedRoute.params
+    // .subscribe(({id}) => {
+    //   console.log(id);
+
+    //   this.paisService.getCountryByCode(id)
+    //   .subscribe(pais=>{
+    //     console.log(pais)
+    //   })
+    // });
+    /* OTRA FORMA CON SWITCH MAP DE RXJS */
     this.activatedRoute.params
-    .subscribe(({id}) => {
-      console.log(id);
-
-      this.paisService.getCountryByCode(id)
-      .subscribe(pais=>{
-        console.log(pais)
-      })
-    });
-
-
+      .pipe(
+        switchMap(({ id }) => this.paisService.getCountryByCode(id)))
+      .subscribe((resp) => {
+        console.log(resp);
+      });
   }
 }
