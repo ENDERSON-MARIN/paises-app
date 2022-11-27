36648e3967c63f98a941547af035dbd9fb49ab6f
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-11-27 00:01:56 -0500

    130. Mostrar sugerencias al escribir - autocomplete

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -5,21 +5,29 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) {}
 
-  buscar(termino:string) {
-    this.termino = termino
-    this.hayError = false
+  buscar(termino: string) {
+    this.termino = termino;
+    this.hayError = false;
+    this.mostrarSugerencias = false;
 
-    this.paisService.buscarPais(this.termino)
-    .subscribe({
+    this.paisService.buscarPais(this.termino).subscribe({
       next: (searchpais) => {
         // console.log(searchpais);
         this.paises = searchpais;
@@ -32,5 +40,25 @@ export class PorPaisComponent {
     });
   }
 
+  sugerencias(termino: string) {
+    this.termino = termino;
+    this.hayError = false;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino).subscribe({
+      next: (searchpais) => {
+        // console.log(searchpais);
+        this.paisesSugeridos = searchpais.splice(0, 5);
+        // console.log('PAISES SUGERIDOS', this.paisesSugeridos);
+      },
+      error: (err) => {
+        console.log(err);
+        this.hayError = true;
+        this.paisesSugeridos = [];
+      },
+    });
+  }
 
+  buscarSugeridos(termino: string) {
+    this.buscar(termino);
+  }
 }
